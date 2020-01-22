06406fefd2da7af5cf4cd07cfadd5ab75020853d
doc-rj <doc.rd.jones@gmail.com>
2020-01-21 19:19:12 -0600

    workaround for this: https://github.com/flutter/flutter/issues/44766

--- digital_rain_clock/android/app/src/main/kotlin/com/example/digital_rain_clock/MainActivity.kt
+++ digital_rain_clock/android/app/src/main/kotlin/com/example/digital_rain_clock/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.digital_rain_clock
 
+import android.os.Build;
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
@@ -8,5 +9,11 @@ import io.flutter.plugins.GeneratedPluginRegistrant
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+
+    override fun onFlutterUiDisplayed() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            reportFullyDrawn();
+        }
     }
 }
